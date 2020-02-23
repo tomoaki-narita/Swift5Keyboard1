2b9794e6705aac3f8516a5c36cfbe7b78bdf6809
output <output.org@gmail.com>
2020-02-23 22:18:03 +0900

    Initial Commit

---
+++ Swift5Keyboard1/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Swift5Keyboard1
+//
+//  Created by output. on 2020/02/23.
+//  Copyright © 2020 output. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
